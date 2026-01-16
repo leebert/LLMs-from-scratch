09d0c327ae7dd177a2751fb961c44fb6c4f64c73
Lee Brenner <brenner.lee@gmail.com>
2026-01-16 12:39:09 -0600

    updated test prompts

--- my-gpt2-774M-sift-model.py
+++ my-gpt2-774M-sift-model.py
@@ -170,21 +170,21 @@ class GPTModel(nn.Module):
     
 def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
 
-    # For-loop is the same as before: Get logits, and only focus on last time step
+    # Get logits, and only focus on last time step
     for _ in range(max_new_tokens):
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
             logits = model(idx_cond)
         logits = logits[:, -1, :]
 
-        # New: Filter logits with top_k sampling
+        # Filter logits with top_k sampling
         if top_k is not None:
             # Keep only top_k values
             top_logits, _ = torch.topk(logits, top_k)
             min_val = top_logits[:, -1]
             logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
 
-        # New: Apply temperature scaling
+        # Apply temperature scaling
         if temperature > 0.0:
             logits = logits / temperature
 
@@ -194,14 +194,14 @@ def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=No
             # Sample from the distribution
             idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
 
-        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        # Otherwise get idx of the vocab entry with the highest logits value
         else:
             idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
 
         if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
             break
 
-        # Same as before: append sampled index to the running sequence
+        # Append sampled index to the running sequence
         idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
 
     return idx
@@ -223,14 +223,13 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(flat.tolist())
 
 
-
 ####################################
 # SETUP
 ####################################
 def format_input(entry):
     instruction_text = (
         f"Below is an instruction that describes a task. "
-        f"\nWrite a response that correctly satisfies the request."
+        f"\nRespond correctly to the request."
         f"\n\n### Instruction:\n{entry['instruction']}"
     )
 
@@ -262,13 +261,14 @@ model.to(device)
 model.eval()
 torch.manual_seed(123)
 
+
 ####################################
 # TEST USAGE
 ####################################
 prompts = [
     {
-        "instruction": "Edit the following sentence for grammar.",
-        "input": "She take the subway every morning."
+        "instruction": "Correct the grammar in the following sentence.",
+        "input": "The dog wag it's tail."
     },
     {
         "instruction": "What is the state capital of Texas?",
